18eec8c259a5449e580b508809aafb8c75f9a878
Rodrigo Puma <99513585+rodrigopuma@users.noreply.github.com>
2025-08-19 20:29:42 -0300

    feat(estrutura): Exibição da matriz melhorada

--- projetos/estrutura.py
+++ projetos/estrutura.py
@@ -35,12 +35,16 @@ matriz = [
 # VETOR [] ROSA
 # LINHA [] AZUL
 
+# EXIBIÇÃO
 for contador_vetor, v in enumerate(matriz, start=1): # vai percorrer a matriz completa voltando com os vetores
-    print(f'{contador_vetor}º Vetor: ', end=' ')
-    for contador_linhas, linhas in enumerate(v): # aqui ele vai percorrer as linhas dos vetores retornando com as linhas de cada um
-        print(linhas, end=' ') if contador_linhas < 2 else print(linhas) # vai printar todas as linhas das do vetor escolhido logo
+    print(f'{contador_vetor}º Vetor:')
+    print('[',end=' ')
+    for contador_linhas, linhas in enumerate(v, start=1): # aqui ele vai percorrer as linhas dos vetores retornando com as linhas de cada um
+        print(f'{contador_linhas}ª linha: ', end=' ')
+        print(linhas, end='      ') if contador_linhas < 3 else print(linhas, end=' ') # vai printar todas as linhas das do vetor escolhido logo
         # FAZER MELHORIAS PARA PERCORRER LINHAS DE UMA FORMA MAIS FACIL
+    print(']', end='\n\n')
     
     # print(v)
     if contador_vetor % 3 == 0:
-        print('\n')
+        print('')
